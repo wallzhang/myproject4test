9a71e7803dfc1377705b8169fd982a81578bccc2
zhang.zhiwen01 <1527891144@qq.com>
2020-11-20 15:14:56 +0800

    测试

--- src/test/java/com/wall/myproject4test/MyProject4testApplicationTest.java
+++ src/test/java/com/wall/myproject4test/MyProject4testApplicationTest.java
@@ -1,21 +1,16 @@
 package com.wall.myproject4test;
 
-import com.alibaba.fastjson.JSON;
 import com.wall.myproject4test.java.zzw.inf4demo.io.MySwaggerInf4Io;
-import net.sf.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.BeansException;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 @SpringBootTest
-class MyProject4testApplicationTests implements ApplicationContextAware {
+class MyProject4testApplicationTest implements ApplicationContextAware {
 
     private static ApplicationContext applicationContext;
 
@@ -53,8 +48,8 @@ class MyProject4testApplicationTests implements ApplicationContextAware {
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        if(MyProject4testApplicationTests.applicationContext == null) {
-            MyProject4testApplicationTests.applicationContext = applicationContext;
+        if(MyProject4testApplicationTest.applicationContext == null) {
+            MyProject4testApplicationTest.applicationContext = applicationContext;
         }
     }
 }
