7b5267e20def6cd08b1ae0bf32e0b5d36885af08
zhang.zhiwen01 <1527891144@qq.com>
2020-12-02 22:39:52 +0800

    简单改下

--- src/test/java/com/wall/myproject4test/Test.java
+++ src/test/java/com/wall/myproject4test/Test.java
@@ -7,19 +7,19 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Test {
     public static void main(String[] args) {
-//        int CAPACITY   = (1 << Integer.SIZE - 3) - 1;
-//        System.out.println(CAPACITY);
-//        System.out.println(536870913 & CAPACITY);
+        int CAPACITY   = (1 << Integer.SIZE - 3) - 1;
+        System.out.println(CAPACITY);
+        System.out.println(536870913 & CAPACITY);
 //        ReentrantReadWriteLock
-        Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                System.out.print("foo");
-            }
-        };
-        Thread thread = new Thread(runnable);
-        thread.start();
-        System.out.print("ball");
-        System.out.println("----------");
+//        Runnable runnable = new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.print("foo");
+//            }
+//        };
+//        Thread thread = new Thread(runnable);
+//        thread.start();
+//        System.out.print("ball");
+//        System.out.println("----------");
     }
 }
